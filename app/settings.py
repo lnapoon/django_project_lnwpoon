98e977203b6761c370d93cb4702143426a2516d1
lnapoon <mpoontv1234@gmail.com>
2026-07-13 10:43:25 +0700

    Move templates and statics inside myweb and fix settings

--- app/settings.py
+++ app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "statics"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
